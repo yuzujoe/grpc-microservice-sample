3028c5eef428b2af8d4f86aee6adfe5ca36f2fe0
yuzujoe <yuzun225@gmail.com>
2021-01-01 23:10:49 +0900

    set up mongo driver

--- blog/server/server.go
+++ blog/server/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gihub.com/yuzujoe/grpc-microservice-sample/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -10,17 +14,40 @@ import (
 	"os/signal"
 )
 
-type server struct {}
+var collection *mongo.Collection
 
-func main()  {
+type server struct{}
+
+type item struct{}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AutherID string `bson:"author_id"`
+	Content  string `bson:"content"`
+	Title    string `bson:"title"`
+}
+
+func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	fmt.Println("Connecting to mongodb")
 	fmt.Println("Blog Service Started")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listern: %v", err)
 	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("mydb").Collection("blog")
 
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
@@ -43,5 +70,7 @@ func main()  {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing mongodb connection")
+	client.Disconnect(context.TODO())
 	fmt.Println("End of program")
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module gihub.com/yuzujoe/grpc-microservice-sample
 go 1.15
 
 require (
+	github.com/aws/aws-sdk-go v1.36.19 // indirect
 	github.com/golang/protobuf v1.4.3
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/klauspost/compress v1.11.4 // indirect
+	go.mongodb.org/mongo-driver v1.4.4 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
 	golang.org/x/text v0.3.4 // indirect
 	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
