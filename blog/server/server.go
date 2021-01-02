449ffccf3ece5cf31489fb7cefc01fe24b061789
yuzujoe <yuzun225@gmail.com>
2021-01-02 16:34:48 +0900

    use evans

--- blog/server/server.go
+++ blog/server/server.go
@@ -10,6 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
@@ -222,6 +223,7 @@ func main() {
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
 	blogpb.RegisterBlogServiceServer(s, &server{})
+	reflection.Register(s)
 
 	go func() {
 		fmt.Println("Starting Server...")
